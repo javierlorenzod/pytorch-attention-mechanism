79738823a8361d30495f6523a776afa13d050d37
Javier Lorenzo Díaz <javier.lorenzod@uah.es>
2021-05-21 10:10:58 +0200

    Update packaging configuration
    Following official tutorial from https://packaging.python.org/tutorials/packaging-projects/

--- setup.py
+++ setup.py
@@ -1,26 +1,30 @@
-from setuptools import setup, find_packages
-import os
-import sys
+import setuptools
 
 
-if sys.version_info[0] < 3:
-    with open('README.md') as f:
-        long_description = f.read()
-else:
-    with open('README.md', encoding='utf-8') as f:
-        long_description = f.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='pytorch-attention',
-    version='1.0-rc-1',
-    description='PyTorch Simple Attention',
+    version='1.0.0-rc-1',
+    description='Simple many-to-one attention in PyTorch',
     author='Javier Lorenzo Díaz',
     url='https://github.com/javierlorenzod/pytorch-attention-mechanism',
-    license='Apache 2.0',
+    project_urls ={
+        "Bug Tracker": "https://github.com/javierlorenzod/pytorch-attention-mechanism/issues",
+    },
+    classifiers =[
+        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3.8"
+        "License :: OSI Approved :: Apache Software License"
+        "Operating System :: OS Independent"
+    ],
     long_description_content_type='text/markdown',
     long_description=long_description,
-    packages=find_packages(), # automatically detect all subpackages and submodules, so long as they contain an __init__.py file
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8.5",
     install_requires=[
         'torch>=1.7.1'
-    ]
+    ],
 )
